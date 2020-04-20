35e50f5771a8ac1c81f1c7ce3d3387d0c3319589
azim254 <verdixconie@gmail.com>
2020-04-20 16:44:12 +0300

    replaced bottom nav with bottom nav view removed travis

--- app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
@@ -10,22 +10,14 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 public class MainActivity extends AppCompatActivity {
 
 
-    private BottomAppBar mBottomAppbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBottomAppbar = findViewById(R.id.bottom_app_bar);
-         setSupportActionBar(mBottomAppbar);    }
 
-    //creating option menu for bottom appbar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-      //  getMenuInflater().inflate(R.menu.bottom_app_bar_menu,menu);
-        return true;
-    }
+        }
+
 
 }
