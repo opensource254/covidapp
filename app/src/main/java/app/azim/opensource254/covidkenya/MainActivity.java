50d4b691578059a1f8b15d2bbb777dce880f176b
azim254 <verdixconie@gmail.com>
2020-04-23 16:09:53 +0300

    set up onclick for news

--- app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
@@ -1,11 +1,16 @@
 package app.azim.opensource254.covidkenya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +21,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CardView mnews = findViewById(R.id.card_news);
+        //starting News activity
+        mnews.setOnClickListener(this::card_btn_news);
 
         }
 
+    //onclick  for news card
+    public void  card_btn_news(View v) {
+        startActivity(new Intent(MainActivity.this, NewsActivity.class));
+    }
+
+    //handling bottom navigation
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                // return true;
+            case R.id.navigation_alerts:
+                //   startActivity(new Intent(MainActivity.this, AlertsActivity.class));
+                //   finish();
+                return true;
+            case R.id.navigation_updates:
+                //  startActivity(new Intent(MainActivity.this, updatesActivity.class));
+                //  finish();
+        }
+        return false;
+    };
 
 }
