b935da06558c43493dc07f70f3faaf9445a3972a
azim254 <verdixconie@gmail.com>
2020-04-23 17:10:47 +0300

    add appbar for news

--- app/src/main/java/app/azim/opensource254/covidkenya/NewsActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/NewsActivity.java
@@ -1,14 +1,33 @@
 package app.azim.opensource254.covidkenya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class NewsActivity extends AppCompatActivity {
 
+
+    //custom  top toolbar
+    private Toolbar mtoolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        //setting up main toolbar
+        mtoolbar = findViewById(R.id.news_tool_bar);
+        setSupportActionBar(mtoolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //setting navigate up button
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+
     }
 }
