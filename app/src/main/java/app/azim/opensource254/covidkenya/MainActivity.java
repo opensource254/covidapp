1a986a771c0fbb7ea0e60fd7720f9ee18548b849
ngangavic <ngangavictor10@gmail.com>
2020-04-23 13:33:35 +0300

    test

--- app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/MainActivity.java
@@ -9,8 +9,6 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
