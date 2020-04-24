e80e6f3346d9f2c84efddfb9a0176eaf3bf31eab
azim254 <verdixconie@gmail.com>
2020-04-24 06:53:09 +0300

    hide status and bottom  bar splash screen

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/SplashActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/SplashActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import app.azim.opensource254.covidkenya.R;
 
@@ -12,6 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //hiding status bar
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar and bottom navbar
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+        //set status to transparent
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_splash);
     updateUI(new Intent(SplashActivity.this, MainActivity.class));
 
