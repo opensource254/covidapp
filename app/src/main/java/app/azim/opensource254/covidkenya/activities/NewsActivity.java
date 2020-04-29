5a1620c947925480fd05456a50e0f27a1de18337
azim254 <verdixconie@gmail.com>
2020-04-30 01:28:20 +0300

    setting up dark items on status bar

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/NewsActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/NewsActivity.java
@@ -3,12 +3,15 @@ package app.azim.opensource254.covidkenya.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -43,6 +46,17 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //setting dark text and white ontouch bottom ui
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        } else {
+            //for lollipop and below use default dark theme
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
 
         //setting up main toolbar
         mtoolbar = findViewById(R.id.news_tool_bar);
