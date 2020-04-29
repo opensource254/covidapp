3f8176af3237c7c170d4e9a70b369727c7275d09
azim254 <verdixconie@gmail.com>
2020-04-30 01:12:41 +0300

    setting up dark items on status bar

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/MainActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/MainActivity.java
@@ -2,10 +2,13 @@ package app.azim.opensource254.covidkenya.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //setting dark text and white ontouch bottom ui
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        } else {
+            //for lollipop and below use default dark theme
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+
 
         CardView mnews = findViewById(R.id.card_news);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
