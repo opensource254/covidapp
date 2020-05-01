92d1d95bb7e67d13295218af50c492a907e539e5
azim254 <verdixconie@gmail.com>
2020-05-01 16:50:49 +0300

    update

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/MainActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -63,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 selectedFragment = new AlertFragment();
                 break;
             case R.id.navigation_situations:
-                selectedFragment = new UpdatesFragment();
+                selectedFragment = new SituationsFragment();
 
         }
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
