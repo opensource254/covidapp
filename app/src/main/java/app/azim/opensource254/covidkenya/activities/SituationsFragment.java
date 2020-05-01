3af0369894b5e6ab546800d3a4db6568238c034e
azim254 <verdixconie@gmail.com>
2020-05-01 16:20:28 +0300

    update

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/SituationsFragment.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/SituationsFragment.java
@@ -2,11 +2,22 @@ package app.azim.opensource254.covidkenya.activities;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.ViewGroup;
+
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import app.azim.opensource254.covidkenya.R;
@@ -18,6 +29,16 @@ public class SituationsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_situation,container,false);
+        View v = inflater.inflate(R.layout.fragment_situation, container, false);
+
+
+
+
+
+        return v;
+
     }
+
+
+
 }
