81c56a1af11dbacb679919b12d97b542231eb97a
azim254 <verdixconie@gmail.com>
2020-05-20 18:35:31 +0300

    modified alert title

---
+++ app/src/main/java/app/azim/opensource254/covidkenya/models/AlertsModel.java
@@ -0,0 +1,17 @@
+package app.azim.opensource254.covidkenya.models;
+
+
+public class AlertsModel {
+    public int id;
+    public String title;
+    public String detail;
+
+    public AlertsModel() {
+    }
+
+    public AlertsModel(int id,  String title , String
+            detail) {
+
+
+    }
+}
