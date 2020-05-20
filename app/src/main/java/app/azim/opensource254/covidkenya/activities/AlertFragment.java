8eed10fd0bc0bee7da75c71962bdc30975310b36
azim254 <verdixconie@gmail.com>
2020-05-20 21:19:34 +0300

    add fetch data alert to main thread

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/AlertFragment.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/AlertFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -40,13 +41,14 @@ import retrofit2.Retrofit;
 
 public class AlertFragment extends Fragment {
 
-    final  static  String mAlertFragment = "AlertFragment";
+    final static String mAlertFragment = "AlertFragment";
     private AlertsRecylerAdapter mrecyclerAdapter;
     private RecyclerView alertsRecyclerView;
     ApiServicesInterface service;
     CompositeDisposable disposable;
     List<AlertsModel> alertsModelList;
     AlertsModel alertModel;
+    private ProgressBar mprogressBar;
     Object response;
 
     //overriding oncreate view
@@ -61,77 +63,61 @@ public class AlertFragment extends Fragment {
         Retrofit mretrofit = ServiceInstance.getRetrofitInstance();
         service = mretrofit.create(ApiServicesInterface.class);
         disposable = new CompositeDisposable();
+        mprogressBar = view.findViewById(R.id.alert_progress_bar);
         alertsRecyclerView = view.findViewById(R.id.alert_recycler_view);
         alertsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         alertsRecyclerView.setHasFixedSize(true);
 
 
-        fetchDataForAlert();
+        try {
+            assert this.getArguments() != null;
+            response = this.getArguments().getSerializable("response");
+        } catch (Exception e) {
+            Log.d(mAlertFragment, "Bundle error: " + e.getMessage());
+            response = null;
+        }
+        alertModel = jsonData(response);
+
+        Log.d(mAlertFragment, "" + alertModel.title);
+        alertsModelList.add(alertModel);
+
+        mrecyclerAdapter = new AlertsRecylerAdapter(alertsModelList, getContext());
+        alertsRecyclerView.setAdapter(mrecyclerAdapter);
+
+        alertsRecyclerView.setVisibility(View.VISIBLE);
+        mprogressBar.setVisibility(View.GONE);
 
         return view;
     }
 
-    private void fetchDataForAlert() {
-        ServiceInstance.getApiService().getAlerts()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Object>() {
-                    @Override
-                    public void onSubscribe(Disposable di) {
-                        disposable.add(di);
-                    }
 
-                    @Override
-                    public void onNext(Object response) {
-                        Log.d(mAlertFragment, "" + response);
-                        alertsModelList = jsonData(response);
-                    }
+    private AlertsModel jsonData(Object response) {
+        alertModel = new AlertsModel();
+        try {
+            JSONObject alertsResponse = new JSONObject(new Gson().toJson(response));
+            JSONArray alertsDataArray = alertsResponse.getJSONArray("data");
+            Log.d(mAlertFragment, "" + alertsDataArray);
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText( getContext(), "Error failed to fetch data", Toast.LENGTH_SHORT).show();
-                        System.out.println("response  Error  " + e.getMessage());
-                    }
+            for (int i = 0; i < alertsDataArray.length(); i++) {
 
-                    @Override
-                    public void onComplete() {
-                        mrecyclerAdapter = new AlertsRecylerAdapter(alertsModelList,  getContext());
-                        alertsRecyclerView.setAdapter(mrecyclerAdapter);
+                JSONObject alertsData = alertsDataArray.getJSONObject(i);
+                Log.d(mAlertFragment, "" + alertsData);
 
-                        alertsRecyclerView.setVisibility(View.VISIBLE);
-                    }
-                });
-
-    }
-
-        private ArrayList<AlertsModel> jsonData(Object response) {
-            alertModel = new AlertsModel();
-            try {
-                JSONObject alertsResponse = new JSONObject(new Gson().toJson(response));
-                JSONArray alertsDataArray = alertsResponse.getJSONArray("data");
-                Log.d(mAlertFragment, "" + alertsDataArray);
-
-                for (int i=0; i < alertsDataArray.length();i++){
-
-                    JSONObject alertsData = alertsDataArray.getJSONObject(i);
-                    Log.d(mAlertFragment, "" + alertsData);
-
-                    int id = alertsData.getInt("id");
-                    String title = alertsData.getString("title");
-                    String detail = alertsData.getString("detail");
+                int id = alertsData.getInt("id");
+                String title = alertsData.getString("title");
+                String detail = alertsData.getString("detail");
 
 
-                    alertModel =
-                            new AlertsModel(0,id,title, detail);
+                alertModel =
+                        new AlertsModel(0, id, title, detail);
 
-                    alertsModelList.add(alertModel);
-                }
-
-            } catch (JSONException e) {
-                Log.d(mAlertFragment, "Json error: " + e.getMessage());
+                alertsModelList.add(alertModel);
             }
-            return (ArrayList<AlertsModel>) alertsModelList;
+        } catch (JSONException e) {
+            Log.d(mAlertFragment, "Json error: " + e.getMessage());
         }
+        return alertModel;
+    }
 
 
 }
