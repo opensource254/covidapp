9ccef03cdf72c9a2ea34c10b3b088ec5852633dc
AustineGwa <austinegwa64@gmail.com>
2020-05-21 20:59:48 -0400

    Build contents viewHolder

--- app/src/main/java/app/azim/opensource254/covidkenya/adapter/viewHolders/AboutContentViewHolder.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/adapter/viewHolders/AboutContentViewHolder.java
@@ -8,8 +8,19 @@ import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder;
 import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder;
 
 import app.azim.opensource254.covidkenya.R;
+import app.azim.opensource254.covidkenya.models.AboutModel;
 
 public class AboutContentViewHolder extends ChildViewHolder {
 
+    private TextView aboutContent;
 
+    public AboutContentViewHolder(View itemView) {
+        super(itemView);
+
+        aboutContent = itemView.findViewById(R.id.about_content);
+    }
+
+    public void onBind(AboutModel aboutModel) {
+       aboutContent.setText(aboutModel.getContent());
+    }
 }
