3e7610073a6d128edcfe47762d4a942a2ae4f3a7
JoelKanyi <joelkanyi98@gmail.com>
2020-06-14 23:07:53 +0300

    worked on the virtual quarantine UI

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/SituationsFragment.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/SituationsFragment.java
@@ -70,12 +70,8 @@ public class SituationsFragment extends Fragment {
         situationRecyclerView.setHasFixedSize(true);
 
         moreStats.setOnClickListener(v1 -> startActivity(new Intent(getActivity(), CountiesStatsActivity.class)));
-<<<<<<< HEAD
-        btngeofence.setOnClickListener(v1 -> startActivity(new Intent(getActivity(),MapsActivity.class)));
-=======
         btngeofence.setOnClickListener(v1 -> startActivity(new Intent(getActivity(),GeofenceActivity.class)));
         virtualQuarantine.setOnClickListener(v1 -> startActivity(new Intent(getActivity(),QuarantineActivity.class)));
->>>>>>> worked on the virtual quarantine UI
 
 
         try {
