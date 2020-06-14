bc4d0c6dc72c9e6ce76d741e991056d32ffad3ab
azim254 <verdixconie@gmail.com>
2020-06-14 15:16:07 +0300

    add geo brodcast receiver

---
+++ app/src/main/java/app/azim/opensource254/covidkenya/brodcastReceivers/GeofenceBroadcastReceiver.java
@@ -0,0 +1,59 @@
+package app.azim.opensource254.covidkenya.brodcastReceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+import app.azim.opensource254.covidkenya.activities.MapsActivity;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "GeofenceBroadcastReceiv";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+//        Toast.makeText(context, "Geofence triggered...", Toast.LENGTH_SHORT).show();
+
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if (geofencingEvent.hasError()) {
+            Log.d(TAG, "onReceive: Error receiving geofence event...");
+            return;
+        }
+
+        List<Geofence> geofenceList = geofencingEvent.getTriggeringGeofences();
+        for (Geofence geofence : geofenceList) {
+            Log.d(TAG, "onReceive: " + geofence.getRequestId());
+        }
+//        Location location = geofencingEvent.getTriggeringLocation();
+        int transitionType = geofencingEvent.getGeofenceTransition();
+
+        switch (transitionType) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_ENTER", Toast.LENGTH_SHORT).show();
+                notificationHelper.sendHighPriorityNotification("You are entering a RED zone", "please take precautions", MapsActivity.class);
+                break;
+            case Geofence.GEOFENCE_TRANSITION_DWELL:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_DWELL", Toast.LENGTH_SHORT).show();
+                notificationHelper.sendHighPriorityNotification("You are in the RED zone", "take measures", MapsActivity.class);
+                break;
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_EXIT", Toast.LENGTH_SHORT).show();
+                notificationHelper.sendHighPriorityNotification("You have exited RED zone", "", MapsActivity.class);
+                break;
+        }
+
+    }
+}
