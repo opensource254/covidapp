7e3b3c5d32aa13dd0b27ab93cfbf745223191822
azim254 <verdixconie@gmail.com>
2020-06-27 00:19:09 +0300

    modified status bar color

--- app/src/main/java/app/azim/opensource254/covidkenya/activities/ExposureNotification/NotifyLearnMoreActivity.java
+++ app/src/main/java/app/azim/opensource254/covidkenya/activities/ExposureNotification/NotifyLearnMoreActivity.java
@@ -1,8 +1,10 @@
 package app.azim.opensource254.covidkenya.activities.ExposureNotification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import app.azim.opensource254.covidkenya.R;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +14,16 @@ public class NotifyLearnMoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify_learn_more);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //setting dark text and white ontouch bottom ui
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        } else {
+            //for lollipop and below use default dark theme
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+
 
         View upButton = findViewById(android.R.id.home);
         upButton.setContentDescription(getString(R.string.navigate_up));
